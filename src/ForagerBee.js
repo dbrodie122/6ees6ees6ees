a04f103e980609444bb15376fe8064716b53a7cc
David Brodie <david.brodie122@gmail.com>
2017-02-22 10:37:23 -0800

    Pass all tests.

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
+class ForagerBee extends HoneyMakerBee{
   // TODO..
+  constructor() {
+  	super();
+  	this.job = 'find pollen';
+  	this.canFly = true;
+  	this.treasureChest = [];
+  }
+  forage(treasure) {
+  	this.treasureChest.push(treasure);
+  }
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee{
   // TODO..
+  constructor(){
+  	super();
+  	this.age = 10;
+  	this.job = 'make honey';
+  	this.honeyPot = 0;
+  }
+  makeHoney() {
+  	return this.honeyPot += 1;
+  }
+  giveHoney() {
+  	return this.honeyPot -= 1;
+  }
 };
